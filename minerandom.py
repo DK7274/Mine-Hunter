bdc377f32946b36f44233a40f27c50a3eb7f991b
Darcy <dkeys8722@gmail.com>
2023-05-18 22:39:29 +1000

    made basic mine randomising system, should probably use arrays all pulled together instead

---
+++ minerandom.py
@@ -0,0 +1,32 @@
+import random
+import array as arr
+
+xMines = [1,1,1,1,1,1,1,1,1,1]
+
+mineCount = 20
+
+yCount = 10
+
+mineRow1 = [0,0,0,0,0,0,0,0,0,0]
+mineRow2 = [0,0,0,0,0,0,0,0,0,0]
+mineRow3 = [0,0,0,0,0,0,0,0,0,0]
+mineRow4 = [0,0,0,0,0,0,0,0,0,0]
+mineRow5 = [0,0,0,0,0,0,0,0,0,0]
+mineRow6 = [0,0,0,0,0,0,0,0,0,0]
+mineRow7 = [0,0,0,0,0,0,0,0,0,0]
+mineRow8 = [0,0,0,0,0,0,0,0,0,0]
+mineRow9 = [0,0,0,0,0,0,0,0,0,0]
+mineRow0 = [0,0,0,0,0,0,0,0,0,0]
+
+yMines = [mineRow1,mineRow2,mineRow3,mineRow4,mineRow5,mineRow6,mineRow7,mineRow8,mineRow9,mineRow0]
+
+
+while mineCount > 0:
+    for n in range(0,len(xMines)):
+        mineChance = random.randint(0,10)
+        if mineChance == 1:
+            xMines[n] += 1
+            mineCount -= 1
+
+
+print(xMines)
