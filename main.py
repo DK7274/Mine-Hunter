378945061bea1955e74e4fac7ba18431146acff9
dkeys <dkeys8722@gmail.com>
2023-05-25 13:14:27 +1000

    finished mine randomiser and put it into the main code as a function, as well as added a bunch of comments into the randomiser sections

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 import pygame
+import random
+
+#variables for mine randomiser
+xMines = [1,1,1,1,1,1,1,1,1,1] #array for n umber of mines down the x Axis
+
+mineCount = 20 #overall amound of mines
+
+mineRow1 = [0,0,0,0,0,0,0,0,0,0] #set of arrays for mine placement
+mineRow2 = [0,0,0,0,0,0,0,0,0,0]
+mineRow3 = [0,0,0,0,0,0,0,0,0,0]
+mineRow4 = [0,0,0,0,0,0,0,0,0,0]
+mineRow5 = [0,0,0,0,0,0,0,0,0,0]
+mineRow6 = [0,0,0,0,0,0,0,0,0,0]
+mineRow7 = [0,0,0,0,0,0,0,0,0,0]
+mineRow8 = [0,0,0,0,0,0,0,0,0,0]
+mineRow9 = [0,0,0,0,0,0,0,0,0,0]
+mineRow0 = [0,0,0,0,0,0,0,0,0,0]
+
+yMines = [mineRow1,mineRow2,mineRow3,mineRow4,mineRow5,mineRow6,mineRow7,mineRow8,mineRow9,mineRow0]
+#array that holds mine arrays within it to be called
+
+#####
 
 # initializing pygame
 pygame.init()
@@ -36,8 +58,30 @@ mine2 = mine(200,200,50,50)
 mine1.draw(screen)
 pygame.display.update()
 
-#def mineSpawn():
+def mineSpawn():
+    global mineCount
+    global yMines
 
+    while mineCount > 0: #sequence to sort the extra 20 mines into different X axis
+        for n in range(0,len(xMines)): #repeats through the array until all mines are used
+            mineChance = random.randint(0,10) #1 in 10 chance of mine being placed per slot
+            if mineChance == 1:
+                if mineCount > 0:
+                    mineCount -= 1
+                    xMines[n] += 1
+
+    for n in range(0,len(yMines)): #sorts through each mine array X axis
+        mineCount = xMines[n]
+        while mineCount > 0: #as long as mine count isn't depleted
+            for m in range(0,len(yMines[n])): #sorts through each individual x axis array
+                if yMines[n][m] == 0: #if no mine there
+                    mineChance = random.randint(0,10) #1 in 10 chance of mine being placed
+                    if mineChance == 1:
+                        if mineCount > 0:
+                            yMines[n][m] += 1
+                            mineCount -= 1
+    print(yMines)
+mineSpawn()
 
 # keep running until quit
 while not gameQuit:
--- minerandom.py
+++ minerandom.py
@@ -1,13 +1,9 @@
 import random
-import array as arr
+xMines = [1,1,1,1,1,1,1,1,1,1] #array for n umber of mines down the x Axis
 
-xMines = [1,1,1,1,1,1,1,1,1,1]
+mineCount = 20 #overall amound of mines
 
-mineCount = 20
-
-yCount = 10
-
-mineRow1 = [0,0,0,0,0,0,0,0,0,0]
+mineRow1 = [0,0,0,0,0,0,0,0,0,0] #set of arrays for mine placement
 mineRow2 = [0,0,0,0,0,0,0,0,0,0]
 mineRow3 = [0,0,0,0,0,0,0,0,0,0]
 mineRow4 = [0,0,0,0,0,0,0,0,0,0]
@@ -19,35 +15,25 @@ mineRow9 = [0,0,0,0,0,0,0,0,0,0]
 mineRow0 = [0,0,0,0,0,0,0,0,0,0]
 
 yMines = [mineRow1,mineRow2,mineRow3,mineRow4,mineRow5,mineRow6,mineRow7,mineRow8,mineRow9,mineRow0]
+#array that holds mine arrays within it to be called
 
-
-while mineCount > 0:
-    for n in range(0,len(xMines)):
-        mineChance = random.randint(0,10)
+while mineCount > 0: #sequence to sort the extra 20 mines into different X axis
+    for n in range(0,len(xMines)): #repeats through the array until all mines are used
+        mineChance = random.randint(0,10) #1 in 10 chance of mine being placed per slot
         if mineChance == 1:
             if mineCount > 0:
                 mineCount -= 1
                 xMines[n] += 1
 
-print(xMines)
-
-m = 0
-
-mineDisplay = 0
-
-for n in range(0,len(yMines)):
+for n in range(0,len(yMines)): #sorts through each mine array X axis
     mineCount = xMines[n]
-    while mineCount > 0:
-        for m in range(0,len(yMines[n])):
-            if yMines[n][m] == 0:
-                mineChance = random.randint(0,10)
+    while mineCount > 0: #as long as mine count isn't depleted
+        for m in range(0,len(yMines[n])): #sorts through each individual x axis array
+            if yMines[n][m] == 0: #if no mine there
+                mineChance = random.randint(0,10) #1 in 10 chance of mine being placed
                 if mineChance == 1:
                     if mineCount > 0:
                         yMines[n][m] += 1
                         mineCount -= 1
 
-
-
-
 print(yMines)
-
